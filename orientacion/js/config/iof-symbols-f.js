93d3cc1bd71bbc5b5f8d347f0dec64356fe4e0f9
juankahf24 <juanka.hf.24@gmail.com>
2026-06-05 16:34:59 +0200

    Add files via upload

---
+++ orientacion/js/config/iof-symbols-f.js
@@ -0,0 +1,14 @@
+/* MILITOPO · Símbolos globales de la columna F
+   Edita este archivo en GitHub para cambiar símbolos en TODOS los dispositivos.
+   Pega el SVG completo entre las comillas invertidas de la clave que quieras.
+   Ejemplo:
+   curva: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 500">...</svg>`
+*/
+window.MILITOPO_IOF_F_SYMBOLS = {
+  // Pega aquí tu SVG exacto de curva cuando quieras fijarlo globalmente.
+  // curva: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 500">...</svg>`,
+
+  // También se pueden sobreescribir si hiciera falta en el futuro:
+  // union: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 500">...</svg>`,
+  // cruce: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 500">...</svg>`
+};
