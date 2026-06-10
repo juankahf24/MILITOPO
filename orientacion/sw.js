9ff71fc6f2b4b6076cc9c0093e983c4d3b743c9d
juankahf24 <juanka.hf.24@gmail.com>
2026-06-10 22:28:58 +0200

    Add files via upload

--- orientacion/sw.js
+++ orientacion/sw.js
@@ -1,5 +1,5 @@
 /* MILITOPO · cache offline seguro v114 · tabla puntos compacta utm estrecha */
-const MILITOPO_CACHE = "militopo-orientacion-modular-v114-tabla-puntos-utm-estrecha";
+const MILITOPO_CACHE = "militopo-orientacion-modular-v115-tabla-puntos-mas-ajustada";
 const CORE_ASSETS = [
   "./",
   "./index.html",
