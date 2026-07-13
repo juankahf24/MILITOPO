8540e3f55e0a276437bcaedbc03cdd2f7e4e2846
juankahf24 <juanka.hf.24@gmail.com>
2026-07-13 11:35:54 +0200

    Add files via upload

--- orientacion/sw.js
+++ orientacion/sw.js
@@ -1,5 +1,5 @@
 /* MILITOPO Orientación · QR salida/llegada compacto */
-const MILITOPO_CACHE = "militopo-orientacion-imported-routes-step3-v3";
+const MILITOPO_CACHE = "militopo-orientacion-step3-print-frame-v1";
 const CORE_ASSETS = [
   "./",
   "./index.html",
