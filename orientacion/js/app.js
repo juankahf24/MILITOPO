6a47489856d2450cec353fb58740db2e23b8eeef
juankahf24 <juanka.hf.24@gmail.com>
2026-07-13 11:36:16 +0200

    Add files via upload

--- orientacion/js/app.js
+++ orientacion/js/app.js
@@ -1,7 +1,7 @@
 /* MILITOPO Orientación · cargador modular seguro fase 2
    No contiene lógica de la app: carga los bloques en orden clásico para mantener compatibilidad. */
 (function(){
-  var VERSION = "imported-routes-step3-v1";
+  var VERSION = "step3-print-frame-v1";
   var files = [
     "js/core/app-main.js",
     "js/pdf/pdf-professional.js",
