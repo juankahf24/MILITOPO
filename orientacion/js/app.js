28be6f3605bd659af06496d7fc8907706d7d2d6d
juankahf24 <juanka.hf.24@gmail.com>
2026-07-15 18:50:11 +0200

    Add files via upload

--- orientacion/js/app.js
+++ orientacion/js/app.js
@@ -1,7 +1,7 @@
 /* MILITOPO Orientación · cargador modular seguro fase 2
    No contiene lógica de la app: carga los bloques en orden clásico para mantener compatibilidad. */
 (function(){
-  var VERSION = "penalizacion-global-v16";
+  var VERSION = "plano-valle-perdido-v2-v17";
   var files = [
     "js/core/app-main.js",
     "js/pdf/pdf-professional.js",
