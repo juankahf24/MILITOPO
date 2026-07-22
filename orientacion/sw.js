7eb0732e51957dc6742faea0b20911e56dd65187
juankahf24 <juanka.hf.24@gmail.com>
2026-07-22 13:37:05 +0200

    Add files via upload

--- orientacion/sw.js
+++ orientacion/sw.js
@@ -1,5 +1,5 @@
 /* MILITOPO Orientación · V63 escala PDF física 1:10.000 / 1:7.500 corregida */
-const MILITOPO_CACHE = "militopo-orientacion-v64-persistencia-track-outbox";
+const MILITOPO_CACHE = "militopo-orientacion-v65-recuperacion-track-carrera-cerrada";
 const CORE_ASSETS = [
   "./",
   "./index.html",
