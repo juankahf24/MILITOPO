736bd20bb9a785a5faf459d7c86c3c139a420eb0
juankahf24 <juanka.hf.24@gmail.com>
2026-07-22 18:21:35 +0200

    Add files via upload

--- orientacion/sw.js
+++ orientacion/sw.js
@@ -1,5 +1,5 @@
 /* MILITOPO Orientación · V63 escala PDF física 1:10.000 / 1:7.500 corregida */
-const MILITOPO_CACHE = "militopo-orientacion-v67-el-valle-matizado";
+const MILITOPO_CACHE = "militopo-orientacion-v68-el-valle-matizado-integrado";
 const CORE_ASSETS = [
   "./",
   "./index.html",
